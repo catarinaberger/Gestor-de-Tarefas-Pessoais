80037b64a63c5da44470e886fd912aabcec43f4e
Catarina Aschenberger <catarina.aschenberger@gmail.com>
2024-12-04 22:12:33 +0000

    Classe tarefa e gestão de status feito

---
+++ tarefa.py
@@ -0,0 +1,27 @@
+
+class Tarefa:
+
+    def __init__(self, titulo, desc, data, status, categ):
+        self.titulo = titulo
+        self.desc = desc
+        self.data = data
+        self.status = "pendente" #começa sempre pendente
+        self.categ = categ
+
+
+    def gerir_status (self):
+        
+        tarefa_concluida = (input("Concluiu a tarefa? S ou N: ")).strip().upper()
+
+        if tarefa_concluida == "S":
+            self.status = "concluída"
+            print(f"A tarefa {self.titulo} foi {self.status}!")
+        elif tarefa_concluida == "N": #só é verificado se o de cima for falso
+            print(f"A tarefa {self.titulo} continuará como {self.status}.")
+        else:
+            print("Erro. Escreva 'S' para sim ou 'N' para não.")
+
+
+
+           
+       
